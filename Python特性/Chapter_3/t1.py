36a8d9adbdf6832cc17ae5f43e7ed758b2218a88
NewBieARM <arman503@yeah.net>
2019-10-14 22:43:52 +0800

    第三章学习进度更新

--- Python特性/Chapter_3/t1.py
+++ Python特性/Chapter_3/t1.py
@@ -1,4 +1,4 @@
-# 1.
+# 1. 函数的嵌套
 def get_speak_func(volume):
     def whisper(text):
         return text.lower() + '...'
@@ -14,11 +14,11 @@ def get_speak_func(volume):
 print(get_speak_func(6)('Hey Is Arman'))
 print(get_speak_func(3)('Hey Is Arman'))
 
-# HEY IS ARMAN!
-# hey is arman...
+# >>  HEY IS ARMAN!
+# >>  hey is arman...
 
 
-# 2.
+# 2. map() 函数的使用
 def to_upper(text):
     return text.upper()
 
@@ -26,10 +26,10 @@ def to_upper(text):
 res = list(map(to_upper, ['arman', 'dawran', 'dilazt']))
 print(res)
 
-# ['ARMAN', 'DAWRAN', 'DILAZT']
+# >>  ['ARMAN', 'DAWRAN', 'DILAZT']
 
 
-# 3.
+# 3. 函数可存储在数据结构中
 def bark(text):
     return text.upper() + '!'
 
@@ -38,6 +38,27 @@ func = [bark, str.lower, str.capitalize]
 for f in func:
     print(f, f('hey, this is arman'))
 
-# <function bark at 0x7f31b393b1e0> HEY, THIS IS ARMAN!
-# <method 'lower' of 'str' objects> hey, this is arman
-# <method 'capitalize' of 'str' objects> Hey, this is arman
+# >>  <function bark at 0x7f31b393b1e0> HEY, THIS IS ARMAN!
+# >>  <method 'lower' of 'str' objects> hey, this is arman
+# >>  <method 'capitalize' of 'str' objects> Hey, this is arman
+
+
+# 4.函数可传递给其他函数
+def bark(text):
+    return text.upper() + '!'
+
+
+def whisper(text):
+    return text.lower() + '...'
+
+
+def greet(func):
+    greeting = func('Hi, I am a Python programmer')
+    print(greeting)
+
+
+greet(bark)
+greet(whisper)
+
+# >>  HI, I AM A PYTHON PROGRAMMER!
+# >>  hi, i am a python programmer...
