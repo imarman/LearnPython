60451f300dfb4dc1518dd147df10edbe66929fde
NewBieARM <arman503@yeah.net>
2019-11-08 22:09:21 +0800

    第四章学习进度更新 -- namedtuple

---
+++ Python特性/Chapter_5/namedtuple (方便的数据对象).py
@@ -0,0 +1,18 @@
+'''
+collections.namedtuple -- 方便的数据对象
+可以当作类使用
+'''
+from collections import namedtuple
+
+Animal = namedtuple('Car', 'name age cry')
+cat = Animal('小猫', 2, 'miao')
+# 有不错的 __repr__ 方法
+print(cat)
+# >>> Car(name='小猫', age=2, cry='miao')
+
+# 访问字段
+cat.cry
+
+# 字段是不可变的
+cat.age = 4
+# >>> AttributeError: can't set attribute
